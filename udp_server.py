013ad8cf384843af7e810674426f72ce06fcbb64
Nkdcg <s.harikrishna6678@gmail.com>
2025-04-07 17:28:58 +0530

    git config --global user.name

--- udp_server.py
+++ udp_server.py
@@ -11,7 +11,7 @@ print(f"🔌 Listening on {UDP_IP}:{UDP_PORT}")
 
 while True:
     data, addr = sock.recvfrom(4096)
-    print(f"📨 Received from {addr}: {data.decode(errors='ignore')}")
+    print(f"📨 Received  from {addr}: {data.decode(errors='ignore')}")
 
     # Send reply (can be audio/text)
     reply = "Acknowledged"
